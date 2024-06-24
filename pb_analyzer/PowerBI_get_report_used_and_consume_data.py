b73a81cb3cfdbc68e11269e75102e5c95a7b851a
Nokod Security <dev@nokodsecurity.com>
2024-06-24 17:57:04 +0300

    Removed utils file for simple execution

--- pb_analyzer/PowerBI_get_report_used_and_consume_data.py
+++ pb_analyzer/PowerBI_get_report_used_and_consume_data.py
@@ -1,11 +1,71 @@
 import argparse
+import csv
 import json
 from urllib.parse import urlparse
 
 import msal
 import requests
 
-from pb_analyzer.utils import filter_strings_not_in_json, write_to_csv, count_hidden_true_in_dict
+
+def filter_strings_not_in_json(strings_list: list, json_string: str):
+    """
+    Filters a list of strings based on whether they are present in a JSON string.
+
+    Args:
+        strings_list (list): A list of strings to filter.
+        json_string (str): A JSON string to compare the strings against.
+    """
+    json_str = json.dumps(json_string)
+    normalized_json_string = json_str.replace('"', '').replace("'", '')
+
+    return [string for string in strings_list if string not in normalized_json_string]
+
+
+def write_to_csv(file_path, values, overwrite=False):
+    """
+    Writes values to a CSV file.
+
+    Args:
+        file_path (str): The path to the CSV file.
+        values (list): The values to write to the CSV file.
+        overwrite (bool): Whether to overwrite the file or append to it. Default is False.
+
+    """
+    mode = 'w' if overwrite else 'a'
+    with open(file_path, mode=mode, newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # write header while deleting past data
+        writer.writerow(values)
+
+
+def count_hidden_true_in_dict(response):
+    """
+    Counts the number of occurrences of the key-value pair "'Hidden': True" in the given dictionary response,
+    ignoring entities where the name starts with 'DateTableTemplate'.
+
+    Parameters:
+    response (dict): The API response as a dictionary.
+
+    Returns:
+    int: The number of occurrences of "'Hidden': True" in the response, excluding entities named 'DateTableTemplate*'.
+    """
+
+    def recursive_count(data, skip_entity=False):
+        count = 0
+        if isinstance(data, dict):
+            for key, value in data.items():
+                # Skip counting if we are in an entity that should be ignored
+                if key == 'Name' and isinstance(value, str) and value.startswith('DateTableTemplate'):
+                    skip_entity = True
+                if key == 'Hidden' and value is True and not skip_entity:
+                    count += 1
+                count += recursive_count(value, skip_entity)
+        elif isinstance(data, list):
+            for item in data:
+                count += recursive_count(item, skip_entity)
+        return count
+
+    return recursive_count(response)
 
 
 def extract_artifact_ids(response: dict):
