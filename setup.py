ad47aad4ca448ed02bd11d57cd578a2f9afda9ce
Nokod Security <dev@nokodsecurity.com>
2025-02-13 15:45:30 +0200

    inc version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='power_bi_analyzer',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
